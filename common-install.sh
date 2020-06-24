36fefe3cb526c7764eb493d2e50c4e85e60957a8
blediagolli <bagolli3@gmail.com>
2020-06-24 19:54:05 -0400

    Update common-install.sh

--- common-install.sh
+++ common-install.sh
@@ -50,37 +50,37 @@ rm -rf /var/cache/yum/*
 rm -rf /var/lib/yum/*
 
 # set home directory
-mkdir -p ${HOME} && \
+#mkdir -p ${HOME} && \
 
 # install gems for target version of fluentd, eventually
 # update to fluentd version that matches version deployed
 # into openshift
-gem install -N --conservative --minimal-deps --no-document \
-  'tzinfo:<1.0.0' \
-  fluentd:${FLUENTD_VERSION} \
-  'activesupport:<5' \
-  'public_suffix:<3.0.0' \
-  'fluent-plugin-record-modifier:<1.0.0' \
-  'fluent-plugin-rewrite-tag-filter:<2.0.0' \
-  fluent-plugin-kubernetes_metadata_filter \
-  fluent-plugin-rewrite-tag-filter \
-  fluent-plugin-secure-forward \
-  'fluent-plugin-remote_syslog:<1.0.0' \
-  fluent-plugin-splunk-ex \
-  fluent-plugin-splunk-hec
+#gem install -N --conservative --minimal-deps --no-document \
+#  'tzinfo:<1.0.0' \
+##  fluentd:${FLUENTD_VERSION} \
+#  'activesupport:<5' \
+#  'public_suffix:<3.0.0' \
+#  'fluent-plugin-record-modifier:<1.0.0' \
+#  'fluent-plugin-rewrite-tag-filter:<2.0.0' \
+#  fluent-plugin-kubernetes_metadata_filter \
+#  fluent-plugin-rewrite-tag-filter \
+#  fluent-plugin-secure-forward \
+#  'fluent-plugin-remote_syslog:<1.0.0' \
+#  fluent-plugin-splunk-ex \
+#  fluent-plugin-splunk-hec
 
 # set up directores so that group 0 can have access like specified in
 # https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html
 # https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html#openshift-specific-guidelines
-mkdir -p /etc/fluent
-chgrp -R 0 /etc/fluent
-chmod -R g+rwX /etc/fluent
-chgrp -R 0 ${HOME}
-chmod -R g+rwX ${HOME}
-chgrp -R 0 /etc/pki
-chmod -R g+rwX /etc/pki
-mkdir /secrets
-chgrp -R 0 /secrets
-chmod -R g+rwX /secrets
-chgrp -R 0 /var/log
-chmod -R g+rwX /var/log
+#mkdir -p /etc/fluent
+#chgrp -R 0 /etc/fluent
+#chmod -R g+rwX /etc/fluent
+#chgrp -R 0 ${HOME}
+#chmod -R g+rwX ${HOME}
+#chgrp -R 0 /etc/pki
+#chmod -R g+rwX /etc/pki
+#mkdir /secrets
+#hgrp -R 0 /secrets
+#chmod -R g+rwX /secrets
+#chgrp -R 0 /var/log
+#hmod -R g+rwX /var/log
