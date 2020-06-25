29b5566d51c194376919932070ad7dc65ef2e4e8
blediagolli <bagolli3@gmail.com>
2020-06-24 20:15:37 -0400

    Update common-install.sh

--- common-install.sh
+++ common-install.sh
@@ -32,12 +32,8 @@ if [[ $RELEASE =~ $CENTOS_MATCH && -z "$USE_SYSTEM_REPOS" ]]; then
   yum install -y epel-release centos-release-scl-rh
 fi
 
-# List enabled repos
-yum repolist enabled
-
 # ensure latest versions
-echo $YUM_ARGS
-yum-config-manager --enable rhel-server-rhscl-7-rpms
+yum-config-manager --enablerepo=rhel-7-server-rpms --enablerepo=rhel-server-rhscl-7-rpms --enablerepo=rhel-7-server-optional-rpms
 
 # install all required packages
 yum list $PACKAGES
@@ -51,37 +47,37 @@ rm -rf /var/cache/yum/*
 rm -rf /var/lib/yum/*
 
 # set home directory
-#mkdir -p ${HOME} && \
+mkdir -p ${HOME} && \
 
 # install gems for target version of fluentd, eventually
 # update to fluentd version that matches version deployed
 # into openshift
-#gem install -N --conservative --minimal-deps --no-document \
-#  'tzinfo:<1.0.0' \
-##  fluentd:${FLUENTD_VERSION} \
-#  'activesupport:<5' \
-#  'public_suffix:<3.0.0' \
-#  'fluent-plugin-record-modifier:<1.0.0' \
-#  'fluent-plugin-rewrite-tag-filter:<2.0.0' \
-#  fluent-plugin-kubernetes_metadata_filter \
-#  fluent-plugin-rewrite-tag-filter \
-#  fluent-plugin-secure-forward \
-#  'fluent-plugin-remote_syslog:<1.0.0' \
-#  fluent-plugin-splunk-ex \
-#  fluent-plugin-splunk-hec
+gem install -N --conservative --minimal-deps --no-document \
+  'tzinfo:<1.0.0' \
+  fluentd:${FLUENTD_VERSION} \
+  'activesupport:<5' \
+  'public_suffix:<3.0.0' \
+  'fluent-plugin-record-modifier:<1.0.0' \
+  'fluent-plugin-rewrite-tag-filter:<2.0.0' \
+  fluent-plugin-kubernetes_metadata_filter \
+  fluent-plugin-rewrite-tag-filter \
+  fluent-plugin-secure-forward \
+  'fluent-plugin-remote_syslog:<1.0.0' \
+  fluent-plugin-splunk-ex \
+  fluent-plugin-splunk-hec
 
 # set up directores so that group 0 can have access like specified in
 # https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html
 # https://docs.openshift.com/container-platform/3.7/creating_images/guidelines.html#openshift-specific-guidelines
-#mkdir -p /etc/fluent
-#chgrp -R 0 /etc/fluent
-#chmod -R g+rwX /etc/fluent
-#chgrp -R 0 ${HOME}
-#chmod -R g+rwX ${HOME}
-#chgrp -R 0 /etc/pki
-#chmod -R g+rwX /etc/pki
-#mkdir /secrets
-#hgrp -R 0 /secrets
-#chmod -R g+rwX /secrets
-#chgrp -R 0 /var/log
-#hmod -R g+rwX /var/log
+mkdir -p /etc/fluent
+chgrp -R 0 /etc/fluent
+chmod -R g+rwX /etc/fluent
+chgrp -R 0 ${HOME}
+chmod -R g+rwX ${HOME}
+chgrp -R 0 /etc/pki
+chmod -R g+rwX /etc/pki
+mkdir /secrets
+chgrp -R 0 /secrets
+chmod -R g+rwX /secrets
+chgrp -R 0 /var/log
+chmod -R g+rwX /var/log
